1b2e1c3627f4e19472da525d3e9157ff703b445b
Eliiss <elizabethoppongansah@gmail.com>
2025-10-12 22:54:47 +0100

    ej 5 almost done

--- A3_P02_Practica0/MATLAB1_EJ_5.m
+++ A3_P02_Practica0/MATLAB1_EJ_5.m
@@ -1,7 +1,4 @@
-% Transformadas de señales
-syms k z a; % Declarar las variables simbólicas (k, z, y la constante a)
-%Symbolic Math Toolbox necesita saber que k, z y a son variables simbólicas
-%para poder realizar las operaciones de forma simbólica
+syms k z a; % Declarar las variables simbólicas (k, z, y la cte a)
 
 % 1.Obtenga la transformada z de la siguiente función: f(k) = 2 + 5k + k^2
 fk1 = 2 + 5*k + k^2;
@@ -9,35 +6,38 @@ Tz1 = ztrans(fk1, k, z); % ztrans calcula la transformada Z
 disp('Transformada Z de f(k) = 2 + 5k + k^2:');
 disp(Tz1);
 
-%Represente gráficamente las señales original y transformada.
-
 % Visualización de la señal original:
-k_valores = 0:10; % Valores discretos de k
+k_valores = 0:20; % Aumentar el número de puntos
 fk1_valores = subs(fk1, k, k_valores); % Sustituir k con los valores para obtener los valores de fk1
 figure;
-subplot(3, 1, 1);
-stem(k_valores, fk1_valores, 'filled'); % 'stem'para representar datos discretos
+subplot(2, 1, 1);
+stem(k_valores, fk1_valores, 'filled'); % stem para representar datos discretos
 title('Señal Original f(k) = 2 + 5k + k^2');
 xlabel('k (Tiempo Discreto)');
 ylabel('Amplitud');
 grid on;
 
 % Visualización de la señal transformada:
-ztrans_plot1 = ezplot(Tz1); 
+subplot(2, 1, 2);
+ezplot(Tz1, [-3, 3, -15, 15]); 
+title('Transformada Z de f(k)');
+xlabel('Re{z}');
+ylabel('Im{z}');
+grid on;
+
 
 % 2.Obtenga la transformada z de la siguiente función: f(k) = sin(k) * e^(-ak)
 fk2 = sin(k) * exp(-a*k);
-Tz2 = ztrans(fk2, k, z); % Calcula la transformada Z
+Tz2 = ztrans(fk2, k, z);
 disp('Transformada Z de f(k) = sin(k) * e^(-ak):');
 disp(Tz2);
 
 % Visualización de la señal original:
-k_valores = 0:10;
-%necesitamos un valor para 'a'
-a_valor = 0.1;  %valor de 'a' (constante)
+k_valores = 0:20; 
+a_valor = 0.1;  %valor de a (cte)
 fk2_valores = subs(fk2, {k, a}, {k_valores, a_valor}); % Sustituir k y a con los valores
 figure;
-subplot(3, 1, 1); 
+subplot(2, 1, 1);
 stem(k_valores, fk2_valores, 'filled');
 title('Señal Original f(k) = sin(k) * e^{-ak}, a = 0.1');
 xlabel('k (Tiempo Discreto)');
@@ -45,7 +45,12 @@ ylabel('Amplitud');
 grid on;
 
 % Visualización de la señal transformada:
-ztrans_plot2 = ezplot(Tz2); 
+subplot(2, 1, 2); % Agregado para la transformada
+ezplot(Tz2, [0, 2, -1, 1]);  % Añadido rango y labels.
+title('Transformada Z de f(k)');
+xlabel('Re{z}');
+ylabel('Im{z}');
+grid on;
 
 % 3. Dada la función de transferencia discreta 
 % T(z) = (0.4*z^2) / (z^3 - z^2 + 0.1z + 0.02)
